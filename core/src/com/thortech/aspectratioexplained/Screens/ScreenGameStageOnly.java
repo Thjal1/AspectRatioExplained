eaadf0d827a1aa0bfd380bed4cc41efaaf2cbb46
Torben Hjalholt <Torben.Hjalholt@cpkelco.com>
2020-10-01 10:24:28 +0200

    Trying to make the addListener workk in Scene2D

--- core/src/com/thortech/aspectratioexplained/Screens/ScreenGameStageOnly.java
+++ core/src/com/thortech/aspectratioexplained/Screens/ScreenGameStageOnly.java
@@ -1,17 +1,19 @@
 package com.thortech.aspectratioexplained.Screens;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.thortech.aspectratioexplained.AbstractGameScreen;
 import com.thortech.aspectratioexplained.Actors.Board;
 import com.thortech.aspectratioexplained.Actors.LibGdXActor;
 import com.thortech.aspectratioexplained.AspectRatioExplained;
+import com.thortech.aspectratioexplained.Helpers.LogHelper;
 import com.thortech.aspectratioexplained.Settings;
 
 /**
@@ -22,6 +24,108 @@ public class ScreenGameStageOnly extends AbstractGameScreen {
 
     private static final String TAG = ScreenGameFill.class.getName();
 
+    private static class TestActor extends Actor {
+        private boolean touched;
+
+        private TestActor() {
+            setBounds(100, 100, 600, 600);
+
+            addListener(new InputListener() {
+                @Override
+                public boolean keyDown(InputEvent event, int keycode) {
+                    touched = true;
+                    return true;
+                }
+
+                @Override
+                public boolean keyUp(InputEvent event, int keycode) {
+                    touched = false;
+                    return true;
+                }
+
+                @Override
+                public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                    touched = true;
+                    return true;
+                }
+
+                @Override
+                public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                    touched = false;
+                }
+            });
+        }
+
+        public void act(float delta) {
+            super.act(delta);
+        }
+
+        @Override
+        public void draw(Batch batch, float parentAlpha) {
+            super.draw(batch, parentAlpha);
+
+            batch.setColor(touched ? Color.GREEN : Color.RED);
+            batch.draw(new Texture(Gdx.files.internal("badlogic.jpg")), getX(), getY(), getWidth(), getHeight());
+        }
+    }
+
+    private final Stage ui = new Stage(new ExtendViewport(1920, 1080));
+
+    public ScreenGameStageOnly(AspectRatioExplained _game) {
+        super(_game);
+        create();
+    }
+
+    protected void create() {
+        //Statics.assets.getMainMenu().finishAndConsume();
+
+//        addInputProcessor(ui);
+        Gdx.input.setInputProcessor(ui);
+        ui.setDebugAll(true);
+        ui.addActor(new TestActor());
+    }
+
+    public void show() {
+        resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    }
+
+
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.706f, 0.851f, 0.847f, 1);
+        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+
+        ui.getViewport().apply();
+        ui.act(delta);
+        ui.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        ui.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+    }
+
+    public void pause() {
+    }
+
+    public void resume() {
+    }
+}
+    /*
     private OrthographicCamera cameraGame;      //For the game play
     private Viewport viewPort;
     private Stage stage;
@@ -35,9 +139,10 @@ public class ScreenGameStageOnly extends AbstractGameScreen {
         viewPort.apply(true);
 
         stage = new Stage(viewPort);
-        Gdx.input.setInputProcessor(stage);
-
         placeActors();
+
+        Gdx.input.setInputProcessor(stage);
+        stage.setDebugAll(true);
     }
 
     public void placeActors()
@@ -45,7 +150,9 @@ public class ScreenGameStageOnly extends AbstractGameScreen {
         LibGdXActor actor = new LibGdXActor();
         actor.setTouchable(Touchable.enabled);
         stage.addActor(actor);
-        //stage.setKeyboardFocus(stage.getActors().first());
+        stage.setKeyboardFocus(stage.getActors().first());
+
+        LogHelper.Log(TAG, "placeActors called", Application.LOG_INFO);
     }
 
     public void handleInput(float dt) {
@@ -65,13 +172,10 @@ public class ScreenGameStageOnly extends AbstractGameScreen {
 
         update(delta);
 
-        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClearColor(0, 0, 0.7f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         Gdx.gl.glEnable(GL20.GL_BLEND);
-
-        stage.act(delta);
-        stage.draw();
 
         //Let the fun begin
         cameraGame.update();
@@ -80,6 +184,9 @@ public class ScreenGameStageOnly extends AbstractGameScreen {
         game.batch.begin();
         //Render sprites
         game.batch.end();
+
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
@@ -91,6 +198,7 @@ public class ScreenGameStageOnly extends AbstractGameScreen {
 
     @Override
     public void show() {
+        resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
     @Override
@@ -109,4 +217,7 @@ public class ScreenGameStageOnly extends AbstractGameScreen {
     public void dispose() {
 
     }
+
 }
+*/
+
